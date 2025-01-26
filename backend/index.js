d38afc6d681e60095e1f0e3b347f477542ab9fec
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-26 15:43:37 +0500

    Mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
 
 const app = express();
+dotenv.config();
 const port = 5000;
 
 app.get("/", (req, res) => {
@@ -8,5 +11,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
+  connectDB();
   console.log(`Server is running on http://localhost:${port}`);
 });
