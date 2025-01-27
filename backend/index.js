39a7fda6b23a12ee6275a82c1df6ddc572c439a6
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-27 18:25:29 +0500

    Add CORS support to backend and update package-lock.json

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/database.js";
 import booksRoute from "./route/booksRoute.js";
 
-const app = express();
 dotenv.config();
+
+const app = express();
 const port = 5000;
 
 app.use(express.json());
+const corsOptions = {
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use("/api/books", booksRoute);
 
