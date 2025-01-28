cf90764fe30e487c7edd111e9b55ff2d3eb357fe
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-28 19:57:03 +0500

    Update BookModel component to accept book props and enhance description

--- frontend/src/components/BookModel.jsx
+++ frontend/src/components/BookModel.jsx
@@ -2,7 +2,7 @@ import { AiOutlineClose } from "react-icons/ai";
 import { PiBookOpenTextLight } from "react-icons/pi";
 import { BiUserCircle } from "react-icons/bi";
 
-const BookModel = () => {
+const BookModel = ({ book, onClose }) => {
   return (
     <div
       className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
@@ -19,7 +19,7 @@ const BookModel = () => {
         <h2 className="w-fit px-4 py-1 bg-red-300 rounded-lg">
           {book.publishYear}
         </h2>
-        <h4 className="my-2 text-gray-500">{book._id}</h4>
+
         <div className="flex justify-start items-center gap-x-2">
           <PiBookOpenTextLight className="text-red-300 text-2xl" />
           <h2 className="my-1">{book.title}</h2>
@@ -28,15 +28,8 @@ const BookModel = () => {
           <BiUserCircle className="text-red-300 text-2xl" />
           <h2 className="my-1">{book.author}</h2>
         </div>
-        <p className="mt-4">Anything You want to show</p>
-        <p className="my-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quia
-          voluptatum sint. Nisi impedit libero eveniet cum vitae qui expedita
-          necessitatibus assumenda laboriosam, facilis iste cumque a pariatur
-          nesciunt cupiditate voluptas? Quis atque earum voluptate dolor nisi
-          dolorum est? Deserunt placeat cumque quo dicta architecto, dolore
-          vitae voluptate sequi repellat!
-        </p>
+        <p className="mt-4">Book Description</p>
+        <p className="my-2">Coming Soon</p>
       </div>
     </div>
   );
