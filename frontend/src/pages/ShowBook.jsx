bb0851c62c0e07dfde2070e90388f4bc57f9b5b8
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-28 19:12:43 +0500

    Update ShowBook component to enhance label clarity for book details

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -35,27 +35,25 @@ const ShowBook = () => {
       ) : book ? (
         <div className="flex flex-col border-2 border-sky-400 rounded-x1 w-fit p-4">
           <div className="my-4">
-            <span className="text-x1 mr-4 text-gray-500">Id</span>
-            <span> {book._id}</span>
-          </div>
-          <div className="my-4">
-            <span className="text-x1 mr-4 text-gray-500">Title</span>
+            <span className="text-x1 mr-4 text-gray-500">Title:</span>
             <span> {book.title}</span>
           </div>
           <div className="my-4">
-            <span className="text-x1 mr-4 text-gray-500">Author</span>
+            <span className="text-x1 mr-4 text-gray-500">Author:</span>
             <span> {book.author}</span>
           </div>
           <div className="my-4">
-            <span className="text-x1 mr-4 text-gray-500">Publish Year</span>
+            <span className="text-x1 mr-4 text-gray-500">Publish Year:</span>
             <span> {book.publishYear}</span>
           </div>
           <div className="my-4">
-            <span className="text-x1 mr-4 text-gray-500">Created At</span>
+            <span className="text-x1 mr-4 text-gray-500">Created At:</span>
             <span> {new Date(book.createdAt).toString()}</span>
           </div>
           <div className="my-4">
-            <span className="text-x1 mr-4 text-gray-500">Last Update Time</span>
+            <span className="text-x1 mr-4 text-gray-500">
+              Last Update Time:
+            </span>
             <span>{new Date(book.updatedAt).toString()}</span>
           </div>
         </div>
